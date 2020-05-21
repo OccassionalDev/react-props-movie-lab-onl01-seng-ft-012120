5885768cada921e9b03c0e5fad6b847bf253056a
Erica Schimmel <ericaschimmel01@gmail.com>
2020-05-21 03:36:13 +0000

    Automatically backed up by Learn

--- src/card-components/CardBack.js
+++ src/card-components/CardBack.js
@@ -13,6 +13,7 @@ export default class CardBack extends Component {
   generateRatingElement = () => {
     // implement meeeee! See the readme for instructions
     
+    if (this.props.)
     return 
   }
 
